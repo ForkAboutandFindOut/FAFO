554c2805791029256d302277c51f2b31f2cbe456
ForkAboutandFindOut <sashavarp7@gmail.com>
2026-01-26 22:13:22 +0400

    Create _episodes.ts

---
+++ functions/_episodes.ts
@@ -0,0 +1,15 @@
+export type Episode = {
+  id: string;
+  title: string;
+  r2_key: string;
+  filename: string;
+};
+
+export const EPISODES: Episode[] = [
+  {
+    id: "ep001",
+    title: "Leon Kuessner",
+    r2_key: "episodes/ep001.mp3",
+    filename: "FAFO_ep001_LeonKuessner.mp3",
+  },
+];
