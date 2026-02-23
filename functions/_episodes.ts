f40eb9191005171af6c5da14b53d7ea2ba116756
Sasha Varpahovsky <sashavarpahovsky@Sashas-MacBook-Pro.local>
2026-02-23 19:24:46 +0400

    Add ep005 metadata for downloads

--- functions/_episodes.ts
+++ functions/_episodes.ts
@@ -24,10 +24,16 @@ export const EPISODES: Episode[] = [
     r2_key: "episodes/ep003.mp3",
     filename: "FAFO_ep003_WillLockwood.mp3",
   },
-   {
+  {
     id: "ep004",
     title: "Tim Issenmann",
     r2_key: "episodes/ep004.mp3",
     filename: "FAFO_ep004_TimIssenmann.mp3",
+  },
+  {
+    id: "ep005",
+    title: "Veer Vij",
+    r2_key: "episodes/ep005.mp3",
+    filename: "FAFO_ep005_VeerVij.mp3",
   },
 ];
